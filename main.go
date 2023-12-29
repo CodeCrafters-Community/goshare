f72e1d600ab2ee53475c5df6499d3b8a2cec6ed9
yzcyayaya <ylzlcl@163.com>
2023-12-29 23:08:02 +0800

    优化多文件上传

--- main.go
+++ main.go
@@ -4,137 +4,116 @@ import (
 	"embed"
 	"flag"
 	"fmt"
-	"io/ioutil"
+	"io"
 	"log"
+	"mime/multipart"
 	"net"
 	"net/http"
 	"os"
 	"os/user"
 	"strings"
+	"sync"
 )
 
 //go:embed html/*
-var multi  embed.FS
+var multi embed.FS
 
 func check(e error) {
 	if e != nil {
 		fmt.Println(e)
 	}
 }
-var(
-	port string
-	view string
+
+var (
+	port         string
+	view         string
 	viewHelpText string
-	path string
-	htmlMap map[string]string
+	path         string
+	htmlMap      map[string]string
 )
 
-func init(){
-	htmlMap =  make(map[string]string)
+func init() {
+	htmlMap = make(map[string]string)
 
 	files, err := multi.ReadDir("html")
-    if err != nil {
-        fmt.Println("Error:", err)
-        os.Exit(1)
-    }
-    var fileName string
-    for _, file := range files {
-        if file.IsDir() {
-            fmt.Println(file.Name()+" is a directory")
-        } else {
+	if err != nil {
+		fmt.Println("Error:", err)
+		os.Exit(1)
+	}
+	var fileName string
+	for _, file := range files {
+		if file.IsDir() {
+			fmt.Println(file.Name() + " is a directory")
+		} else {
 			//分割字符取第一个
-			fileName = strings.Split(file.Name(),".")[0]
+			fileName = strings.Split(file.Name(), ".")[0]
 			viewHelpText = viewHelpText + fileName + "\t"
-            htmlMap[fileName] = file.Name()
-        }
-    }
+			htmlMap[fileName] = file.Name()
+		}
+	}
 	viewHelpText = viewHelpText + "\ndisplay *.html"
 }
 
-
 func main() {
 	currentUser, err := user.Current()
-    if err != nil {
-       log.Fatalf(err.Error())
-    }
-	
-	err = os.MkdirAll(currentUser.HomeDir+"/Desktop/upfile",os.ModePerm)
-	if err!=nil{
+	if err != nil {
+		log.Fatalf(err.Error())
+	}
+
+	err = os.MkdirAll(currentUser.HomeDir+"/Desktop/upfile", os.ModePerm)
+	if err != nil {
 		log.Fatal(err)
 	}
 	flag.StringVar(&port, "p", "9000", "port example: -p 9000;端口 例子: -p 9000")
-	flag.StringVar(&view, "v", "simple",viewHelpText)
-	flag.StringVar(&path, "P", currentUser.HomeDir+"/Desktop/upfile","download/upload directory 下载/上传目录")
-	fmt.Println("默认上传/下载路径：\t"+currentUser.HomeDir+"/Desktop/upfile")
+	flag.StringVar(&view, "v", "simple", viewHelpText)
+	flag.StringVar(&path, "P", currentUser.HomeDir+"/Desktop/upfile", "download/upload directory 下载/上传目录")
+	fmt.Println("默认上传/下载路径：\t" + currentUser.HomeDir + "/Desktop/upfile")
 	flag.Parse()
 	fmt.Println("请访问下面的链接:")
-	showip()
+	showIp()
 	http.HandleFunc("/", uploadFileHandler)
 	http.Handle("/file/", http.StripPrefix("/file/", http.FileServer(http.Dir(path))))
 	http.ListenAndServe(":"+port, nil)
 }
-func nameToView(view string)string{
-	content ,err := multi.ReadFile("html/"+view)
+func nameToView(view string) string {
+	content, err := multi.ReadFile("html/" + view)
 	if err != nil {
-        panic(err)
-    }
-    return string(content)
+		panic(err)
+	}
+	return string(content)
 }
 
 func uploadFileHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, nameToView(htmlMap[view]))
-	uploadOne(w, r)
-}
-
-func uploadOne(w http.ResponseWriter, r *http.Request) {
-	if r.Method == "POST" {
-		file, handler, err := r.FormFile("file_uploads") //name的字段
-		check(err)
-		defer file.Close()
-		fileBytes, err := ioutil.ReadAll(file)
-		check(err)
-		newFile, err := os.Create(path + "/" + handler.Filename)
-		check(err)
-		defer newFile.Close()
-		if _, err := newFile.Write(fileBytes); err != nil {
-			check(err)
-			return
-		}
-		fmt.Println(" upload successfully:" + path + "/" + handler.Filename)
-		w.Write([]byte("SUCCESS"))
+	switch r.Method {
+	case "POST":
+		upload(w, r)
 	}
 }
-func uploadMore(w http.ResponseWriter, r *http.Request) {
-	if r.Method == "POST" {
-		log.Println("======================进来了")
-		//设置内存大小
-		// r.ParseMultipartForm(32 << 20)
-		//获取上传的文件组
-		files := r.MultipartForm.File["file"]
-		log.Println(files, "==================")
-		for _, fileItem := range files {
-			//打开上传文件
-			file, err := fileItem.Open()
-			fileBytes, err := ioutil.ReadAll(file)
 
-			defer file.Close()
-			check(err)
+func writeFile(wg *sync.WaitGroup, file *os.File, multiFile multipart.File) {
+	defer wg.Done()
+	io.Copy(file, multiFile)
+}
 
-			//创建上传文件
-			cur, err := os.Create(path + "/" + fileItem.Filename)
-			fmt.Println("上传地址:path/")
-			check(err)
-			defer cur.Close()
-			if _, err := cur.Write(fileBytes); err != nil {
-				check(err)
-				return
-			}
-			fmt.Println(" upload successfully:" + path + "/" + fileItem.Filename)
-			w.Write([]byte("SUCCESS"))
-		}
+func upload(w http.ResponseWriter, r *http.Request) {
+	_ = r.ParseMultipartForm(1 << 30)
+	var wg sync.WaitGroup
+	files := r.MultipartForm.File["file"]
+	for _, file := range files {
+		create, err := os.Create(path + "/" + file.Filename)
+		check(err)
+		wg.Add(1)
+		open, err := file.Open()
+		check(err)
+		go writeFile(&wg, create, open)
+		wg.Wait()
+		fmt.Println(" upload successfully:" + path + "/" + file.Filename)
+		w.Write([]byte("</br></br>" + file.Filename + "&nbsp;SUCCESS</br>"))
 	}
 }
-func showip() {
+
+func showIp() {
 	addrs, err := net.InterfaceAddrs()
 	if err != nil {
 		fmt.Println(err)
@@ -142,9 +121,9 @@ func showip() {
 	}
 	for _, address := range addrs {
 		// 检查ip地址判断是否回环地址
-		if ipnet, ok := address.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
-			if ipnet.IP.To4() != nil {
-				fmt.Println("http://"+ipnet.IP.String() + ":" + port)
+		if ipNet, ok := address.(*net.IPNet); ok && !ipNet.IP.IsLoopback() {
+			if ipNet.IP.To4() != nil {
+				fmt.Println("http://" + ipNet.IP.String() + ":" + port)
 			}
 		}
 	}
